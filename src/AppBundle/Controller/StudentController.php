40cf9332a0b3494cf1d9c7da9b7d7b827dc4b798
MwDylanD <d.dufay@mentalworks.fr>
2017-11-23 09:16:10 +0100

    some change

---
+++ src/AppBundle/Controller/StudentController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Student;
+use AppBundle\Form\RegisterSelectorType;
+use AppBundle\Form\StudentType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * Student controller.
+ *
+ * @Route("student")
+ */
+class StudentController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return RedirectResponse|Response
+     *
+     * @Route("/", name="student_index")
+     */
+    public function indexAction(Request $request)
+    {
+        $form = $this->createForm(RegisterSelectorType::class, null);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                /** @var Student $student */
+                $student = $form->getData()->getStudent();
+                if (null === $student) {
+                    throw new \Exception('Utilisateur non trouvé');
+                }
+                return $this->redirectToRoute('student_show', ['id' => $student->getId()]);
+            } catch (\Exception $e) {
+                $this->addFlash('danger', 'L\'élève n\'a pas été trouvé');
+            }
+        }
+        return $this->render('student/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     *
+     * @Route("/new", name="student_add")
+     */
+    public function addAction(Request $request)
+    {
+        $student = new Student();
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                dump($student);
+                $em->persist($student);
+                $em->flush();
+                $this->addFlash('success', 'Élève ajouté');
+                return $this->redirectToRoute('student_show', ['id' => $student->getId()]);
+            } catch (\Exception $e) {
+                dump($e->getMessage());
+                die;
+                $this->addFlash('danger', 'Erreur durant l\'ajout d\'un élève');
+            }
+        }
+        return $this->render('student/new.html.twig', [
+            'student' => $student,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @param Student $student
+     * @param Request $request
+     * @return Response
+     * @throws \Exception
+     *
+     * @Route("/{id}", name="student_show")
+     */
+    public function showAction(Student $student, Request $request)
+    {
+        if (null === $student) {
+            throw new \Exception('Élève non trouvé');
+        }
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($student);
+                $em->flush();
+                $this->addFlash('success', 'Élève modifié');
+
+                return $this->redirectToRoute('student_index');
+            } catch (\Exception $e) {
+                $this->addFlash('danger', 'Erreur lors de la modification');
+            }
+        }
+
+        return $this->render('student/show.html.twig', [
+            'student' => $student,
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/AppBundle/Form/StudentType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Student;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class StudentType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastName', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'E-mail'
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Numéro de téléphone'
+            ])
+            ->add('certificate', CertificateObtentionType::class, [
+                'data_class' => null,
+            ]);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}
